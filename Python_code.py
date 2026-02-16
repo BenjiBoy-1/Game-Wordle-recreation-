b50d928ceb1e2ec6471024e3140d4be7a96e4c75
BenjiBoy-1 <bergheim.benjamin@emanuelschool.nsw.edu.au>
2026-02-16 14:50:46 +1100

    we did it

--- Python_code.py
+++ Python_code.py
@@ -19,43 +19,49 @@ def load_apple_wordlist():
 words = load_apple_wordlist()
 
 five = [w for w in words if len(w) == 5]
+
+"""
 print(len(five))
 print(five[:])
+
+"""
 
 
 # print random letter and split into a list
 answer = random.choice(five)
-print(answer)
-answer = list(answer)
-print(answer)
+# print(answer)
+answer_list = list(answer)
+# print(answer_list)
 
+num_guesses = 0
 
 guess = input("What's your first guess? ")
-letters = list(guess)
-print("You entered:", guess)
+while guess != answer and num_guesses < 4:
+    letters = list(guess)
+    print("You entered:", guess)
 
-for x in range(len(answer)):
-    # correct letter, correct spot
-    if letters[x] == answer[x]:
-        print(f"{Color.GREEN}{letters[x]}{Color.END}", end="")
+    for x in range(len(answer_list)):
+        # correct letter, correct spot
+        if letters[x] == answer_list[x]:
+            print(f"{Color.GREEN}{letters[x]}{Color.END}", end="")
 
-    # letter exists somewhere in word
-    elif letters[x] in answer:
-        print(f"{Color.YELLOW}{letters[x]}{Color.END}", end="")
+        # letter exists somewhere in word
+        elif letters[x] in answer_list:
+            print(f"{Color.YELLOW}{letters[x]}{Color.END}", end="")
 
-    # not in word
-    else:
-        print(letters[x], end="")
+        # not in word
+        else:
+            print(letters[x], end="")
 
-print()   # newline
-
-
-print()
+    print()   # newline
 
 
-print("\033[92mHELLO\033[0m")
-print("NORMAL")
+    print()
+    num_guesses = num_guesses + 1
+    guess = input("What's your next guess? ")
+
+if num_guesses > 3:
+    print(f"You lose lmao the word was {Color.RED}{answer}{Color.END}. Better luck next time!")
     
- 
-
-print("Letters:", letters)
+elif guess == answer:
+    print(f"Congrats you guessed the word correctly and the word was {Color.GREEN}{answer}{Color.END}. And unlike NYT wordle, you can play again today!!!")
